9635367d3735a382bc08fd1f45dbc8c1d4cbc299
Frederico Valente <psytek@localhost.localdomain>
2014-02-13 14:41:08 +0000

    init changes

--- bridge.py
+++ bridge.py
@@ -47,7 +47,7 @@ class Bridge:
 	def modules(self):
 		self.ssh.sendline(agent+"-m")
 		self.ssh.prompt()
-		files = self.ssh.before.replace(".py", "").split()[3:] #first 3 splits are python aget.py -m
+		files = self.ssh.before.split()[3:] #first 3 splits are python aget.py -m
 		return files
 
 	def jobs(self):
@@ -69,16 +69,10 @@ class Bridge:
 
 if __name__ == "__main__":
 
-	x = Bridge()
+	bridge = Bridge()
 
 	print("listing modules:")
-	print(x.modules())
-	print(type(x.modules()))
-	print(len(x.modules()))
-	for item in x.modules():
-		print(item)
-		print(type(item))
-	print(x.modules()[0])
+	print(bridge.modules())
 
 	#print(x.jobs())
 	#print(x.scheduleWorkflow())
