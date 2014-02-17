f087d8b732a05b9b156fa27a43837c044422851d
Frederico Valente <psytek@localhost.localdomain>
2014-02-17 12:06:30 +0000

    settings file

---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+
+
+#general configuration
+agent_path = os.path.dirname(os.path.realpath(__file__))
+modules_path = os.path.dirname(os.path.realpath(__file__))+"/modules"
+workflows_path = os.path.dirname(os.path.realpath(__file__))+"/workflows"
+persistent_state_path = os.path.dirname(os.path.realpath(__file__))+"/persistentstate"
+execution_path = os.path.dirname(os.path.realpath(__file__))+"/var"
