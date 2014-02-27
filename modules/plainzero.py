606373b462097d8723287f857c4075f6d79f4518
Frederico Valente <psytek@localhost.localdomain>
2014-02-27 15:28:52 +0000

    a test file

---
+++ modules/plainzero.py
@@ -0,0 +1,27 @@
+import subprocess
+import settings
+import logging
+
+def launch(output_dir):
+    
+    logging.info("launching text_printer to:"+output_dir)
+
+    file = open(output_dir+"/launch.sh",'w')
+
+    script = "#!/bin/sh\n"
+    script += "hostname>{}/ff\n".format(output_dir)
+    script += "touch {}\n".format(output_dir+"/_state_finished")
+    script += "python {} -ud {}".format(settings.agent_path+"/agent.py", output_dir)
+
+    file.write(script)
+    file.close()
+
+    subprocess.Popen(["sh", output_dir+"/launch.sh"])
+
+
+def collect(output_dir):
+    print ("collect init from:"+output_dir)
+
+
+if __name__ == "__main__":
+    print("no test here for main...")
